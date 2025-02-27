159ce6d39ce0c1d35d2dda92c02c9c4eaf943d94
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-02-27 19:35:26 +0530

    footer add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructure from "@/components/Instructure";
 import MusiclCards from "@/components/MusiclCards";
@@ -14,6 +15,7 @@ export default function Home() {
       <MusiclCards />
       <UpcomingWebinars />
       <Instructure/>
+      <Footer/>
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,41 @@
+
+function Footer() {
+  return (
+    <footer className="bg-black text-gray-400 py-12">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 px-3 sm:px-6 lg:px-8">
+         <div className="flex flex-col max-w-40">
+            <div className="text-bold text-white text-lg mb-3">
+               About Us
+            </div>
+            <p>Music School is a premier institution dedicated to teching the art and since of</p>
+         </div>
+         <div className="flex flex-col  ">
+            <div className="text-bold text-white text-lg mb-3">
+               Quick Links
+            </div>
+            <p>Home</p>
+            <p>About</p>
+            <p>Course</p>
+            <p>Contact</p>
+         </div>
+         <div className="flex flex-col  ">
+            <div className="text-bold text-white text-lg mb-3">
+               Follow Us
+            </div>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+         </div>
+         <div className="flex flex-col  ">
+            <div className="text-bold text-white text-lg mb-3">Contect Us</div>
+            <p>Mumbai, India</p>
+            <p>Mumbai 400066</p>
+            <p>Email: info@musicschool.com</p>
+         </div>
+      </div>
+      <p className="text-center text-xs pt-8 tracking-normal font-semibold">© 2025 Music School. All right reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer

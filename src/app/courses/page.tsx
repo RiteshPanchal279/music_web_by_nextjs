8c81b6a043831622da38c8a7209c7a80f45beb99
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-03-02 21:54:59 +0530

    button

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -40,7 +40,7 @@ function page() {
                 <CardItem
                   translateZ={20}
                   as={Link}
-                  href="https://twitter.com/mannupaaji"
+                  href="https://www.linkedin.com/in/ritesh-panchal-2191482b2/"
                   target="__blank"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
